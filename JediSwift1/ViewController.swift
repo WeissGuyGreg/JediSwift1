deb97d41c970dee20060ab6f87dd9327d581ab3f
wiseguy16 <gergusa04@gmail.com>
2016-10-29 16:52:58 -0400

    Added a new label

--- JediSwift1/ViewController.swift
+++ JediSwift1/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newView = UILabel()
+        newView.text = "Hello All Jedis"
+        newView.textAlignment = .Center
+        newView.frame = CGRect(x: 30, y: 200, width: 150, height: 40)
+        newView.backgroundColor = UIColor.grayColor()
+        view.addSubview(newView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
